60ae49a989ab20f923d169d832092f3731593e75
artelabsuper <artelabsuper@gmail.com>
2019-04-09 16:36:04 +0200

    * bash script improved

--- train-on-ferramenta.sh
+++ train-on-ferramenta.sh
@@ -14,11 +14,18 @@ do
         echo "Downloading the dataset..."
         curl -O ${link}
         tar xfz ${file}
-    fi
+   fi
 done
 
-mv images-train train
-mv images-val val
+if [[ -f images-train ]]
+then
+    mv images-train train
+fi
+
+if [[ -f images-val ]]
+then
+    mv images-val val
+fi
 
 echo "Starting training process..."
 python3 train_model.py training_flags.csv
