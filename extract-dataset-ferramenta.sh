8848e5bb77bba246fb34442a6774f625e7282749
artelabsuper <artelabsuper@gmail.com>
2019-04-09 15:21:04 +0200

    * changing bash scripts

--- extract-dataset-ferramenta.sh
+++ extract-dataset-ferramenta.sh
@@ -1,23 +1,3 @@
 #!/usr/bin/env bash
 
-w=10 # embedding width
-encoding_height=1 # embedding height
-ste_image_w=227
-ste_separator_size=4
-ste_superpixel_size=3
-
-echo "output_image_width $w"
-echo "encoding_height $encoding_height"
-python3 extract_encoded_images.py \
-                 --train_path "${HOME}/datasets/ferramenta52-multimodal/train.csv"  \
-                 --val_path "${HOME}/datasets/ferramenta52-multimodal/val.csv"  \
-                 --output_dir "${HOME}/datasets/ferramenta52-multimodal/new_blank_encoding/${w}x${w}-${encoding_height}-sep${ste_separator_size}-spixel${ste_superpixel_size}" \
-                 --output_image_width ${w} \
-                 --encoding_height ${encoding_height} \
-                 --save_model_dir_name "runs/ferramenta52-$w-$encoding_height" \
-                 --gpu_id "0,1,2" \
-                 --batch_size 64 \
-                 --ste_image_w ${ste_image_w} \
-                 --ste_separator_size ${ste_separator_size} \
-                 --ste_superpixel_size ${ste_superpixel_size}
-
+python3 extract_encoded_images.py extraction_flags.csv
--- run-experiments-ferramenta.sh
+++ run-experiments-ferramenta.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Downloading the dataset..."
+wget http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/images-val.tar.gz
+wget http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/images-train.tar.gz
+wget http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/text-train.tar.gz
+wget http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/text-val.tar.gz
+
+echo "Uncompressing the dataset..."
+tar -xvfz images-val.tar.gz
+tar -xvfz images-train.tar.gz
+tar -xvfz text-train.tar.gz
+tar -xvfz text-val.tar.gz
+
+echo "Starting training process..."
 python3 train_model.py training_flags.csv
+echo "Done!"
