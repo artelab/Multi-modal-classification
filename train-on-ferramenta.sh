02e75baff1782a5064fc5122a1627d731724f4bf
artelabsuper <artelabsuper@gmail.com>
2019-04-10 14:05:30 +0200

    * changed bash script

--- train-on-ferramenta.sh
+++ train-on-ferramenta.sh
@@ -2,7 +2,9 @@
 
 
 declare -a arr=("http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/images-train.tar.gz"
-                "http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/images-val.tar.gz")
+                "http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/images-val.tar.gz"
+                "http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/text-images-train.csv"
+                "http://artelab.dista.uninsubria.it/downloads/datasets/commercial_offers/ferramenta/text-images-val.csv")
 
 for link in "${arr[@]}"
 do
@@ -19,13 +21,18 @@ done
 
 if [[ -f images-train ]]
 then
+    tar xfz images-train.tar.gz
     mv images-train train
 fi
 
 if [[ -f images-val ]]
 then
+    tar xfz images-train.tar.gz
     mv images-val val
 fi
+
+mv text-images-train.csv train.csv
+mv text-images-val.csv val.csv
 
 echo "Starting training process..."
 python3 scripts/train_model.py training_parameters.csv
